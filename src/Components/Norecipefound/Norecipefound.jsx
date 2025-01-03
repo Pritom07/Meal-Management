ae7afc6da7229fdf57f44cb6de6210d9c5a43e57
Pritom07 <pritommohajon4545@gmail.com>
2025-01-03 21:17:46 +0600

    create responsive the Norecipefound component

--- src/Components/Norecipefound/Norecipefound.jsx
+++ src/Components/Norecipefound/Norecipefound.jsx
@@ -1,9 +1,9 @@
 const Norecipefound = () => {
     return (
-        <div className="flex flex-col justify-center items-center">
-            <img src='searchfail.png' className="w-[60%]" />
+        <div className="flex flex-col justify-center items-center mt-2">
+            <img src='searchfail.png' className="w-[60%] md:w-[30%]" />
             <h1 className="text-3xl font-semibold text-red-700 text-center">Oops no related recipe found !</h1>
-        </div>
+        </div >
     );
 };
 
