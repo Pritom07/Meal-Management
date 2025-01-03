4c8785110a9e89c2e1c04c43dfd7d19afdd29cba
Pritom07 <pritommohajon4545@gmail.com>
2025-01-03 14:30:07 +0600

    The project is successfully complite and responsive for all kinds of devices

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -21,7 +21,17 @@ const About = () => {
                     <img className="float-left w-[15%]" src="logo.png" />
                     <p className="text-xl font-medium ml-1 text-justify"><span className="text-violet-900 text-3xl font-semibold">Meal-Management</span> is a cutting-edge web application crafted with love to assist <span className="text-violet-900 text-2xl font-semibold">My mother</span> as a gift <span className="text-violet-900 text-2xl font-semibold">in her birthday</span> from me and home cooks in transforming her cooking experience. Imagine effortlessly discovering a treasure trove of recipes from around the world, complete with step-by-step preparation methods, detailed ingredient lists, and even insights into each dish’s cultural origins. This app goes beyond being just a recipe finder; it brings cuisines from various regions to your fingertips, enabling users to explore and experiment with diverse flavors and cooking styles. Whether you’re curious about popular dishes in your area or looking to try something new from another corner of the globe, Meal-Management has you covered. A standout feature of this app is its focus on recipe popularity and in-depth statistics, which guide users to make informed decisions when planning meals. Want to know what’s trending? Or perhaps you’re looking for tried-and-tested recipes loved by many? It’s all here. Meal-Management is more than a tool; it’s a companion in the kitchen. By simplifying meal planning and making cooking stress-free, it turns mealtime into a delightful adventure for families worldwide. Built with the intent to honor the love, creativity, and dedication of mothers in their kitchens, this app celebrates their invaluable role in nurturing families through the joy of food.</p>
 
-                    <p className="text-xl font-medium ml-1 text-justify md:flex items-center mt-2">To get the instructions about how the <span className="text-xl font-semibold text-violet-900 m-1 text-nowrap">MEAL-MANAGEMENT</span> uses , please click here. <span className="flex items-center mt-2 md:mt-0"><FaArrowRightLong className="ml-2" /> <Modal></Modal></span></p>
+                    <p className="text-xl font-medium ml-1 text-justify flex flex-wrap items-center mt-2 gap-1">
+                        To get the instructions about how the
+                        <span className="text-xl font-semibold text-violet-900 whitespace-nowrap">MEAL-MANAGEMENT</span>
+                        works, please click here.
+                        <span className="flex items-center">
+                            <FaArrowRightLong className="ml-2" />
+                            <Modal />
+                        </span>
+                    </p>
+
+
 
                     <h1 className="text-3xl font-semibold mt-5">Some Frequently Asked Questions :</h1>
                     <Accordion></Accordion>
@@ -32,3 +42,5 @@ const About = () => {
 };
 
 export default About;
+
+{/* <p className="text-xl font-medium ml-1 text-justify md:flex items-center mt-2">To get the instructions about how the <span className="text-xl font-semibold text-violet-900 m-1 text-nowrap">MEAL-MANAGEMENT</span> uses , please click here. <span className="flex items-center mt-2 md:mt-0"><FaArrowRightLong className="ml-2" /> <Modal></Modal></span></p> */ }
--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -22,121 +22,90 @@ const Statistics = () => {
             {
                 (loading) ? ((<div className="flex justify-center items-center h-screen">
                     <PacmanLoader color="#8F00FF" size={35} />
-                </div>)) : (<div className="flex flex-col-reverse md:flex-row items-center justify-center md:mt-7 w-full">
-                    <div className="md:w-2/3 w-full h-[400px] px-4 flex justify-center items-center">
-                        <ResponsiveContainer>
-                            <LineChart
-                                width={Math.min(800, window.innerWidth * 0.9)}
-                                height={400}
-                                data={data}
-                                margin={{ top: 50, right: 20, left: 20, bottom: 20 }} >
-                                <CartesianGrid strokeDasharray="5 5" />
-                                <XAxis
-                                    dataKey="country"
-                                    tickFormatter={formatCountryName}
-                                    stroke="#000000"
-                                    interval={data.length > 8 ? Math.floor(data.length / 4) : 0}
-                                    tick={{
-                                        fontSize: window.innerWidth < 640 ? 10 : 12,
-                                        textAnchor: "middle",
-                                    }}
-                                    tickLine={false}
-                                    dy={5}
-                                />
-                                <YAxis stroke="#000000" />
-                                <Tooltip />
-                                <Legend
-                                    wrapperStyle={{
-                                        position: "absolute",
-                                        top: -6,
-                                        right: 0,
-                                        fontSize: window.innerWidth < 640 ? "10px" : "12px",
-                                        marginTop: "25px",
-                                        marginLeft: "10px",
-                                    }}
-                                />
-                                <Line type="monotone" dataKey="foodPopularity" stroke="#8F00FFFF" />
-                            </LineChart>
-                        </ResponsiveContainer>
-                    </div >
+                </div>)) : (<div className="md:mt-9 min-h-screen">
+                    <div className="flex flex-col-reverse md:flex-row items-center justify-center w-full">
+                        <div className="md:w-2/3 w-full h-[400px] px-4 flex justify-center items-center">
+                            <ResponsiveContainer>
+                                <LineChart
+                                    width={Math.min(800, window.innerWidth * 0.9)}
+                                    height={400}
+                                    data={data}
+                                    margin={{ top: 50, right: 20, left: 20, bottom: 20 }} >
+                                    <CartesianGrid strokeDasharray="5 5" />
+                                    <XAxis
+                                        dataKey="country"
+                                        tickFormatter={formatCountryName}
+                                        stroke="#000000"
+                                        interval={data.length > 8 ? Math.floor(data.length / 4) : 0}
+                                        tick={{
+                                            fontSize: window.innerWidth < 640 ? 10 : 12,
+                                            textAnchor: "middle",
+                                        }}
+                                        tickLine={false}
+                                        dy={5}
+                                    />
+                                    <YAxis stroke="#000000" />
+                                    <Tooltip />
+                                    <Legend
+                                        wrapperStyle={{
+                                            position: "absolute",
+                                            top: -6,
+                                            right: 0,
+                                            fontSize: window.innerWidth < 640 ? "10px" : "12px",
+                                            marginTop: "25px",
+                                            marginLeft: "10px",
+                                        }}
+                                    />
+                                    <Line type="monotone" dataKey="foodPopularity" stroke="#8F00FFFF" />
+                                </LineChart>
+                            </ResponsiveContainer>
+                        </div >
 
-                    <div className="md:w-1/3 w-full px-4 mt-5 md:mt-0">
-                        <table className="border-collapse table-fixed w-full text-center">
-                            <caption className="text-lg font-bold mb-4">Country Name and their Abbreviation</caption>
-                            <tbody>
-                                <tr>
-                                    <td className="border border-slate-700 p-2">JAP <FaArrowRightLong className="inline mx-2" /> Japanese</td>
-                                    <td className="border border-slate-700 p-2">CRO <FaArrowRightLong className="inline mx-2" /> Croatian</td>
-                                </tr>
-                                <tr>
-                                    <td className="border border-slate-700 p-2">TUR <FaArrowRightLong className="inline mx-2" /> Turkish</td>
-                                    <td className="border border-slate-700 p-2">EGY <FaArrowRightLong className="inline mx-2" /> Egyptian</td>
-                                </tr>
-                                <tr>
-                                    <td className="border border-slate-700 p-2">FIL <FaArrowRightLong className="inline mx-2" /> Filipino</td>
-                                    <td className="border border-slate-700 p-2">CHI <FaArrowRightLong className="inline mx-2" /> Chinese</td>
-                                </tr>
-                                <tr>
-                                    <td className="border border-slate-700 p-2">TUN <FaArrowRightLong className="inline mx-2" /> Tunisian</td>
-                                    <td className="border border-slate-700 p-2">AME <FaArrowRightLong className="inline mx-2" /> American</td>
-                                </tr>
-                                <tr>
-                                    <td className="border border-slate-700 p-2">ITA <FaArrowRightLong className="inline mx-2" /> Italian</td>
-                                    <td className="border border-slate-700 p-2">CAN <FaArrowRightLong className="inline mx-2" /> Canadian</td>
-                                </tr>
-                                <tr>
-                                    <td className="border border-slate-700 p-2">IND <FaArrowRightLong className="inline mx-2" /> Indian</td>
-                                    <td className="border border-slate-700 p-2">DUT <FaArrowRightLong className="inline mx-2" /> Dutch</td>
-                                </tr>
-                                <tr>
-                                    <td className="border border-slate-700 p-2">GRE <FaArrowRightLong className="inline mx-2" /> Greek</td>
-                                    <td className="border border-slate-700 p-2">BRI <FaArrowRightLong className="inline mx-2" /> British</td>
-                                </tr>
-                                <tr>
-                                    <td className="border border-slate-700 p-2">FRE <FaArrowRightLong className="inline mx-2" /> French</td>
-                                    <td className="border border-slate-700 p-2">MAL <FaArrowRightLong className="inline mx-2" /> Malaysian</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <h1></h1>
-                </div >)
+                        <div className="md:w-1/3 w-full px-4 mt-5 md:mt-0">
+                            <table className="border-collapse table-fixed w-full text-center">
+                                <caption className="text-lg font-bold mb-4">Country Name and their Abbreviation</caption>
+                                <tbody>
+                                    <tr>
+                                        <td className="border border-slate-700 p-2">JAP <FaArrowRightLong className="inline mx-2" /> Japanese</td>
+                                        <td className="border border-slate-700 p-2">CRO <FaArrowRightLong className="inline mx-2" /> Croatian</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="border border-slate-700 p-2">TUR <FaArrowRightLong className="inline mx-2" /> Turkish</td>
+                                        <td className="border border-slate-700 p-2">EGY <FaArrowRightLong className="inline mx-2" /> Egyptian</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="border border-slate-700 p-2">FIL <FaArrowRightLong className="inline mx-2" /> Filipino</td>
+                                        <td className="border border-slate-700 p-2">CHI <FaArrowRightLong className="inline mx-2" /> Chinese</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="border border-slate-700 p-2">TUN <FaArrowRightLong className="inline mx-2" /> Tunisian</td>
+                                        <td className="border border-slate-700 p-2">AME <FaArrowRightLong className="inline mx-2" /> American</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="border border-slate-700 p-2">ITA <FaArrowRightLong className="inline mx-2" /> Italian</td>
+                                        <td className="border border-slate-700 p-2">CAN <FaArrowRightLong className="inline mx-2" /> Canadian</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="border border-slate-700 p-2">IND <FaArrowRightLong className="inline mx-2" /> Indian</td>
+                                        <td className="border border-slate-700 p-2">DUT <FaArrowRightLong className="inline mx-2" /> Dutch</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="border border-slate-700 p-2">GRE <FaArrowRightLong className="inline mx-2" /> Greek</td>
+                                        <td className="border border-slate-700 p-2">BRI <FaArrowRightLong className="inline mx-2" /> British</td>
+                                    </tr>
+                                    <tr>
+                                        <td className="border border-slate-700 p-2">FRE <FaArrowRightLong className="inline mx-2" /> French</td>
+                                        <td className="border border-slate-700 p-2">MAL <FaArrowRightLong className="inline mx-2" /> Malaysian</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div >
+                    <h1 className="text-center font-semibold text-2xl mt-4">Fig : Line Chart Showing Food Popularity Across Various Countries</h1>
+                    <h1 className="mt-5 text-xl font-medium text-justify">The line chart displayed on the website visualizes the popularity of different cuisines from various countries. The horizontal axis (X-axis) represents the countries, while the vertical axis (Y-axis) shows the popularity scores, measured on a scale from 0 to 100. Each point on the chart corresponds to a country's cuisine, with a connecting line to indicate trends or patterns across the data. For example, cuisines like Italian, French, and Indian show high popularity scores above 90, while Tunisian and Croatian cuisines have lower scores, indicating a lesser popularity compared to others. This chart helps viewers quickly identify and compare the popularity of different international cuisines at a glance.</h1>
+                </div>)
             }
         </div >
     );
 };
-
 export default Statistics;
-
-{/* <LineChart
-                            width={Math.min(800, window.innerWidth * 0.9)}
-                            height={400}
-                            data={data}
-                            margin={{ top: 40, right: 20, left: 30, bottom: 20 }} >
-                            <CartesianGrid strokeDasharray="5 5" />
-                            <XAxis
-                                dataKey="country"
-                                tickFormatter={formatCountryName}
-                                stroke="#000000"
-                                interval={data.length > 8 ? Math.floor(data.length / 4) : 0}
-                                tick={{
-                                    fontSize: window.innerWidth < 640 ? 10 : 12,
-                                    textAnchor: "middle",
-                                }}
-                                tickLine={false}
-                                dy={5}
-                            />
-                            <YAxis stroke="#000000" />
-                            <Tooltip />
-                            <Legend
-                                wrapperStyle={{
-                                    position: "absolute",
-                                    top: -10,
-                                    right: 0,
-                                    fontSize: window.innerWidth < 640 ? "10px" : "12px",
-                                    marginTop: "25px",
-                                    marginLeft: "10px",
-                                }}
-                            />
-                            <Line type="monotone" dataKey="foodPopularity" stroke="#8F00FFFF" />
-                        </LineChart> */}
